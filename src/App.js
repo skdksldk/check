dd7229ee64f7e3ce01b719892e1edf507ef61cf0
skdksldk <tnwns1485@naver.com>
2023-11-11 20:40:42 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [number, setNumber] = useState(2);
+  const [result, setResult] = useState(0);
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setNumber(value);
+  };
+
+  const handleCalculate = () => {
+    setResult(number * number); // 버그: 2를 고정값으로 사용하고 있음
+  };
+
   return (
-    <div className="App">
-          akakakaka
+    <div>
+      <h1>구구단 계산기</h1>
+      <input type="number" value={number} onChange={handleInputChange} />
+      <button onClick={handleCalculate}>계산</button>
+      <p>{`${number} * 2 = ${result}`}</p>
     </div>
   );
-}
+};
 
 export default App;
